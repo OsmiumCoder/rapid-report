40c1bdd5d5222cdda276c817b4418a7147551b19
Liam Welsh <liamwelsh17@gmail.com>
2025-01-10 13:32:56 -0400

    Adds methods to incident model for getting incident submitter and supervisor

--- app/Models/Incident.php
+++ app/Models/Incident.php
@@ -10,6 +10,16 @@ class Incident extends Model
     /** @use HasFactory<\Database\Factories\IncidentFactory> */
     use HasFactory;
 
+    public function submitter()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function supervisor()
+    {
+        return $this->belongsTo(User::class, 'supervisor_id');
+    }
+
     public function witnesses()
     {
         return $this->belongsToMany(User::class, 'witnesses', 'incident_id', 'witness_id');
