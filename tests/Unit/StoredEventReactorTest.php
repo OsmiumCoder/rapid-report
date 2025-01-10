74a3c4fbe2b978d2413c885ab3c4acf104b4a551
Jonathon Meney <jonmeney@gmail.com>
2025-01-09 20:13:54 -0400

    base event handling tests

---
+++ tests/Unit/StoredEventReactorTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Reactors\StoredEventReactor;
+use App\StorableEvents\StoredEvent;
+use Mockery;
+use Mockery\Adapter\Phpunit\MockeryTestCase;
+
+class StoredEventReactorTest extends MockeryTestCase
+{
+    public function test_stored_event_react_method_called() {
+        $mock = Mockery::spy(StoredEvent::class);
+
+        $mock->shouldReceive("react")->once();
+
+        $mock->shouldNotReceive("handle");
+
+        $this->getProjector()->onStoredEvent($mock);
+
+        $mock->shouldHaveReceived("react");
+
+        $mock->shouldNotHaveReceived("handle");
+    }
+
+    private function getProjector() {
+        return new StoredEventReactor;
+    }
+}
