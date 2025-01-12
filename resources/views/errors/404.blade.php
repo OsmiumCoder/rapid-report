d0605990884d71a8574eca4641b272bd34b66a8b
Jonathon Meney <jonmeney@gmail.com>
2025-01-11 21:21:41 -0400

    adjusted layout of 404 page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,19 +1,28 @@
-@vite('resources/css/app.css')
 <!DOCTYPE html>
-<html>
+<html class="h-full">
 <head>
-    <title>404</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Not Found</title>
+
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
+
+    <!-- Scripts -->
+    @vite(['resources/js/app.tsx'])
 </head>
-<body>
-    <div class="flex min-h-full flex-col">
-        <main class="mx-auto flex w-full max-w-7xl flex-auto flex-col justify-center px-6 py-24 sm:py-64 lg:px-8">
-            <p class="text-base/8 font-semibold text-violet-400">404</p>
-            <h1 class="mt-4 text-pretty text-5xl font-semibold tracking-tight text-gray-900 sm:text-6xl">Page not found</h1>
-            <p class="mt-6 text-pretty text-lg font-medium text-gray-500 sm:text-xl/8">Sorry, we couldn’t find the page you’re looking for.</p>
-            <div class="mt-10">
-                <a href="{{ route('dashboard') }}" class="text-sm/7 font-semibold text-violet-400"><span aria-hidden="true">&larr;</span> Back to home</a>
-            </div>
-        </main>
+<body class="h-full">
+<main class="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+    <div class="text-center">
+        <p class="text-base font-semibold text-indigo-600">404</p>
+        <h1 class="mt-4 text-balance text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl">Page not found</h1>
+        <p class="mt-6 text-pretty text-lg font-medium text-gray-500 sm:text-xl/8">Sorry, we couldn’t find the page you’re looking for.</p>
+        <div class="mt-10 flex items-center justify-center gap-x-6">
+            <a href="{{ route('dashboard') }}" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Go back home</a>
+            <a href="#" class="text-sm font-semibold text-gray-900">Contact support <span aria-hidden="true">&rarr;</span></a>
+        </div>
     </div>
+</main>
 </body>
 </html>
