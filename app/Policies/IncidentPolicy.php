5450310f1e9ac50bfaaff2c443727c39fa9541da
Jonathon Meney <jonmeney@gmail.com>
2025-01-13 20:15:43 -0400

    policy class

---
+++ app/Policies/IncidentPolicy.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Incident;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class IncidentPolicy
+{
+    /**
+     * Perform pre-authorization checks.
+     */
+    public function before(User $user, string $ability): bool|null
+    {
+        if ($user->isAdministrator()) {
+            return true;
+        }
+
+        return null;
+    }
+
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, Incident $incident): bool
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Incident $incident): bool
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, Incident $incident): bool
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, Incident $incident): bool
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, Incident $incident): bool
+    {
+        //
+    }
+}
