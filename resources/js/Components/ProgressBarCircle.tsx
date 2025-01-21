1e799c60ef47e6af1d8275c599280d91cd074c83
Jonathon Meney <jonmeney@gmail.com>
2025-01-21 00:50:15 -0400

    removed dead code

--- resources/js/Components/ProgressBarCircle.tsx
+++ resources/js/Components/ProgressBarCircle.tsx
@@ -90,69 +90,6 @@ export default function ProgressBarCircle({
                         </a>
                     </li>
                 ))}
-
-                {/*{steps.map((step, stepIdx) => (*/}
-                {/*    <li key={step.name}>*/}
-                {/*        {step.status === 'complete' ? (*/}
-                {/*            <>*/}
-                {/*                <div*/}
-                {/*                    aria-hidden="true"*/}
-                {/*                    className="absolute inset-0 flex items-center"*/}
-                {/*                >*/}
-                {/*                    <div className="h-0.5 w-full bg-indigo-600" />*/}
-                {/*                </div>*/}
-                {/*                <a*/}
-                {/*                    href="#"*/}
-                {/*                    className="relative flex size-8 items-center justify-center rounded-full bg-indigo-600 hover:bg-indigo-900"*/}
-                {/*                >*/}
-                {/*                    <CheckIcon*/}
-                {/*                        aria-hidden="true"*/}
-                {/*                        className="size-5 text-white"*/}
-                {/*                    />*/}
-                {/*                </a>*/}
-                {/*            </>*/}
-                {/*        ) : step.status === 'current' ? (*/}
-                {/*            <>*/}
-                {/*                <div*/}
-                {/*                    aria-hidden="true"*/}
-                {/*                    className="absolute inset-0 flex items-center"*/}
-                {/*                >*/}
-                {/*                    <div className="h-0.5 w-full bg-gray-200" />*/}
-                {/*                </div>*/}
-                {/*                <a*/}
-                {/*                    href="#"*/}
-                {/*                    aria-current="step"*/}
-                {/*                    className="relative flex size-8 items-center justify-center rounded-full border-2 border-indigo-600 bg-white"*/}
-                {/*                >*/}
-                {/*                    <span*/}
-                {/*                        aria-hidden="true"*/}
-                {/*                        className="size-2.5 rounded-full bg-indigo-600"*/}
-                {/*                    />*/}
-                {/*                    <span className="sr-only">{step.name}</span>*/}
-                {/*                </a>*/}
-                {/*            </>*/}
-                {/*        ) : (*/}
-                {/*            <>*/}
-                {/*                <div*/}
-                {/*                    aria-hidden="true"*/}
-                {/*                    className="absolute inset-0 flex items-center"*/}
-                {/*                >*/}
-                {/*                    <div className="h-0.5 w-full bg-gray-200" />*/}
-                {/*                </div>*/}
-                {/*                <a*/}
-                {/*                    href="#"*/}
-                {/*                    className="group relative flex size-8 items-center justify-center rounded-full border-2 border-gray-300 bg-white hover:border-gray-400"*/}
-                {/*                >*/}
-                {/*                    <span*/}
-                {/*                        aria-hidden="true"*/}
-                {/*                        className="size-2.5 rounded-full bg-transparent group-hover:bg-gray-300"*/}
-                {/*                    />*/}
-                {/*                    <span className="sr-only">{step.name}</span>*/}
-                {/*                </a>*/}
-                {/*            </>*/}
-                {/*        )}*/}
-                {/*    </li>*/}
-                {/*))}*/}
             </ol>
         </nav>
     );
