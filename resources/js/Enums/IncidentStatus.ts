a05e5df7a699956c627ee99765e080cc75be4b4e
Liam Welsh <liamwelsh17@gmail.com>
2025-01-27 16:36:55 -0400

    Readds IncidentStatus Enum in Frontend as it will be needed for future tasks

---
+++ resources/js/Enums/IncidentStatus.ts
@@ -0,0 +1,7 @@
+export enum IncidentStatus {
+    OPENED = 'Opened',
+    CLOSED = 'Closed',
+    REOPENED = 'Re-Opened',
+    ASSIGNED = 'Assigned',
+    INREVIEW = 'In-Review',
+}
--- resources/js/types/Incident.ts
+++ resources/js/types/Incident.ts
@@ -1,3 +1,5 @@
+import { IncidentStatus } from '@/Enums/IncidentStatus';
+
 export interface Incident {
     id: string;
     anonymous: boolean;
@@ -23,7 +25,7 @@ export interface Incident {
     reporters_email?: string;
     supervisor_name?: string;
     supervisor_id?: number;
-    status: string;
+    status: IncidentStatus;
     closed_at?: string;
     created_at: string;
     updated_at: string;
