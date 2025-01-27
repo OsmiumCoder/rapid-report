1790912828f25dad034092e27247949359f5f54f
Jonathon Meney <jonmeney@gmail.com>
2025-01-27 19:55:54 -0400

    comment model with relations and factory

---
+++ database/factories/CommentFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Enum\CommentType;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CommentFactory extends Factory
+{
+
+    public function definition(): array
+    {
+        return [
+            'type' => CommentType::NOTE,
+            'content' => fake()->paragraph(2),
+        ];
+    }
+}
