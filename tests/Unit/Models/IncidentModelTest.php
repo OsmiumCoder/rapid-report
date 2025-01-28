dec9e3549434495bc213ec43dd0035563f38a2e9
Liam Welsh <liamwelsh17@gmail.com>
2025-01-27 22:34:57 -0400

    Adds Incident model test to return supervisor user object

--- tests/Unit/Models/IncidentModelTest.php
+++ tests/Unit/Models/IncidentModelTest.php
@@ -3,10 +3,21 @@
 namespace Tests\Unit\Models;
 
 use App\Models\Incident;
+use App\Models\User;
 use Tests\TestCase;
 
 class IncidentModelTest extends TestCase
 {
+    public function test_incident_with_assigned_supervisor_returns_supervisor_user()
+    {
+        $supervisor = User::factory()->create()->assignRole('supervisor');
+        $incident = Incident::factory()->create([
+            'supervisor_id' => $supervisor->id,
+        ]);
+
+        $this->assertEquals($supervisor->id, $incident->supervisor->id);
+    }
+
     public function test_creates_an_incident_model_with_valid_attributes()
     {
         $incident = Incident::factory()->create();
