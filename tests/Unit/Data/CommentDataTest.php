2f2ac07f2aba47989ec3b272a8ad4cedf223c941
Jonathon Meney <jonmeney@gmail.com>
2025-01-28 01:54:41 -0400

    added test for comment data

--- tests/Unit/Data/CommentDataTest.php
+++ tests/Unit/Data/CommentDataTest.php
@@ -2,9 +2,35 @@
 
 namespace Data;
 
+use App\Data\CommentData;
+use Illuminate\Validation\ValidationException;
 use Tests\TestCase;
 
 class CommentDataTest extends TestCase
 {
+    public function test_incident_data_valid_with_correct_values(): void
+    {
 
+        $commentData = CommentData::validateAndCreate([
+            'content' => 'comments',
+        ]);
+
+        $this->assertInstanceOf(CommentData::class, $commentData);
+    }
+
+    public function test_comment_data_throws_invalid_if_empty_comment(): void
+    {
+        $this->expectException(ValidationException::class);
+
+        CommentData::validateAndCreate([
+            'content' => '',
+        ]);
+    }
+
+    public function test_comment_data_throws_invalid_if_no_comment(): void
+    {
+        $this->expectException(ValidationException::class);
+
+        CommentData::validateAndCreate([]);
+    }
 }
