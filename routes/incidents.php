4673b96b1f0208d688d1b587d3733ecc7b00eb8e
Liam Welsh <liamwelsh17@gmail.com>
2025-01-27 22:47:28 -0400

    Updates incident routes

--- routes/incidents.php
+++ routes/incidents.php
@@ -12,12 +12,12 @@ Route::resource('incidents', IncidentController::class)->only([
 ]);
 
 Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/incidents/owned', OwnedIncidentsController::class)->name('incidents.owned');
+    Route::get('/incidents/assigned', AssignedIncidentsController::class)->name('incidents.assigned');
+    Route::put('/incidents/{incident}/assign', AssignSupervisorController::class)->name('incidents.assign-supervisor');
+
     Route::resource('incidents', IncidentController::class)->except([
         'create',
         'store',
     ]);
-
-    Route::get('/owned', OwnedIncidentsController::class)->name('incidents.owned');
-    Route::get('/assigned', AssignedIncidentsController::class)->name('incidents.assigned');
-    Route::put('/incidents/{incident}/assign', AssignSupervisorController::class)->name('incidents.assign-supervisor');
 });
