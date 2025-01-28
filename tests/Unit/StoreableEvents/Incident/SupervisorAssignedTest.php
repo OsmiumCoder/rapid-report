19271cdd1524af0c0050de82fae7a5b6b72c4e7c
Liam Welsh <liamwelsh17@gmail.com>
2025-01-27 23:19:51 -0400

    Updates test

--- tests/Unit/StoreableEvents/Incident/SupervisorAssignedTest.php
+++ tests/Unit/StoreableEvents/Incident/SupervisorAssignedTest.php
@@ -5,6 +5,7 @@ namespace Tests\Unit\StoreableEvents\Incident;
 use App\Models\Incident;
 use App\Models\User;
 use App\States\IncidentStatus\Assigned;
+use App\States\IncidentStatus\Opened;
 use App\StorableEvents\Incident\SupervisorAssigned;
 use Tests\TestCase;
 
@@ -14,6 +15,7 @@ class SupervisorAssignedTest extends TestCase
     {
         $supervisor = User::factory()->create()->assignRole('supervisor');
         $incident = Incident::factory()->create();
+        $this->assertEquals(Opened::class, $incident->status::class);
 
         $event = new SupervisorAssigned($supervisor->id);
         $event->setAggregateRootUuid($incident->id);
