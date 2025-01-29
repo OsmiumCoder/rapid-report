8792dc5cfcfc28327ebac95a4ea0e7ff27fc600e
Liam Welsh <liamwelsh17@gmail.com>
2025-01-28 22:41:17 -0400

    Fixes dropdown on supervisor assign to have same height as button

--- resources/js/Pages/Incident/Partials/AdminActionComponents/SupervisorUpdate.tsx
+++ resources/js/Pages/Incident/Partials/AdminActionComponents/SupervisorUpdate.tsx
@@ -53,7 +53,7 @@ export default function SupervisorUpdate({
     };
 
     return (
-        <div className="mt-6 border-t border-gray-900/5 py-6 px-9">
+        <div className="mt-6 border-t border-gray-900/5 py-6 px-4">
             <div className="flex flex-wrap items-center w-full gap-x-4">
                 <div className="w-24 text-gray-900 font-medium">
                     Supervisor:
@@ -62,8 +62,8 @@ export default function SupervisorUpdate({
                     onChange={(id) => assignSupervisor(parseInt(id))}
                     disabled={isLoading}
                 >
-                    <div className="relative">
-                        <ListboxButton className="grid min-w-32 w-full cursor-default grid-cols-1 rounded-md bg-white py-1.5 pl-3 pr-2 text-left text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
+                    <div className="relative max-w-lg">
+                        <ListboxButton className="inline-flex items-center justify-between min-w-32 h-8 w-full cursor-default grid-cols-1 rounded-md bg-white pl-3 pr-2 text-left text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm">
                             <span className="col-start-1 row-start-1 truncate pr-6">
                                 {
                                     supervisors.find(
