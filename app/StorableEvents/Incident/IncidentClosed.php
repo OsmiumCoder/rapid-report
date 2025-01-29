9b2d633b9ce4406948d2c3191cea8d41f5a1d637
Jonathon Meney <jonmeney@gmail.com>
2025-01-29 18:35:55 -0400

    updated events to create action comment on unassign, reopen, and closed

--- app/StorableEvents/Incident/IncidentClosed.php
+++ app/StorableEvents/Incident/IncidentClosed.php
@@ -2,6 +2,8 @@
 
 namespace App\StorableEvents\Incident;
 
+use App\Enum\CommentType;
+use App\Models\Comment;
 use App\Models\Incident;
 use App\States\IncidentStatus\Closed;
 use App\StorableEvents\StoredEvent;
@@ -13,5 +15,15 @@ class IncidentClosed extends StoredEvent
         $incident = Incident::find($this->aggregateRootUuid());
         $incident->status->transitionTo(Closed::class);
         $incident->save();
+
+        $comment = new Comment;
+
+        $comment->user_id = $this->metaData['user_id'] ?? null;
+        $comment->type = CommentType::ACTION;
+        $comment->content = 'Incident was closed.';
+
+        $comment->commentable()->associate($incident);
+
+        $comment->save();
     }
 }
--- app/StorableEvents/Incident/IncidentReopened.php
+++ app/StorableEvents/Incident/IncidentReopened.php
@@ -2,6 +2,8 @@
 
 namespace App\StorableEvents\Incident;
 
+use App\Enum\CommentType;
+use App\Models\Comment;
 use App\Models\Incident;
 use App\States\IncidentStatus\Reopened;
 use App\StorableEvents\StoredEvent;
@@ -14,5 +16,15 @@ class IncidentReopened extends StoredEvent
         $incident->status->transitionTo(Reopened::class);
         $incident->supervisor_id = null;
         $incident->save();
+
+        $comment = new Comment;
+
+        $comment->user_id = $this->metaData['user_id'] ?? null;
+        $comment->type = CommentType::ACTION;
+        $comment->content = 'Incident was reopened.';
+
+        $comment->commentable()->associate($incident);
+
+        $comment->save();
     }
 }
