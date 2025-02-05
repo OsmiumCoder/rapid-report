ee4e8b5c3fba9e390d321ab15cd08d6dfe1ea44a
Liam Welsh <liamwelsh17@gmail.com>
2025-02-04 22:48:16 -0400

    Updates supervisor assignment dropdown to be searchable

--- resources/js/Pages/Incident/Partials/AdminActionComponents/SupervisorUpdate.tsx
+++ resources/js/Pages/Incident/Partials/AdminActionComponents/SupervisorUpdate.tsx
@@ -2,6 +2,12 @@ import { User } from '@/types';
 import { uppercaseWordFormat } from '@/Filters/uppercaseWordFormat';
 import { Incident } from '@/types/incident/Incident';
 import {
+    Combobox,
+    ComboboxButton,
+    ComboboxInput,
+    ComboboxOption,
+    ComboboxOptions,
+    Label,
     Listbox,
     ListboxButton,
     ListboxOption,
@@ -19,6 +25,7 @@ import ConfirmationModal, {
     ConfirmationModalProps,
     useConfirmationModalProps,
 } from '@/Components/ConfirmationModal';
+import { CheckIcon, MagnifyingGlassIcon } from '@heroicons/react/20/solid';
 
 export default function SupervisorUpdate({
     incident,
@@ -29,99 +36,113 @@ export default function SupervisorUpdate({
 }) {
     const [isLoading, setIsLoading] = useState(false);
     const [modalProps, setModalProps] = useConfirmationModalProps();
+    const [query, setQuery] = useState('');
+    const [defaultText, setDefaultText] = useState('Unassigned');
+
+    const filteredSupervisors =
+        query === ''
+            ? supervisors
+            : supervisors.filter((supervisor) => {
+                  return (
+                      supervisor.name.toLowerCase().includes(query.toLowerCase()) ||
+                      supervisor.email.toLowerCase().includes(query.toLowerCase())
+                  );
+              });
 
     return (
         <>
-            <div className="mt-6 w-full border-t border-gray-900/5 pt-6 px-4">
-                <div className="flex flex-wrap justify-center items-center w-full gap-x-4">
-                    <div className="w-24 text-gray-900 font-medium">
-                        Supervisor:
-                    </div>
-                    <Listbox
-                        onChange={(id) => {
-                            const supervisor = supervisors.find(
-                                (supervisor) => supervisor.id === parseInt(id)
-                            );
+            <label className="block text-sm/6 font-medium text-gray-900 mt-6">
+                Supervisor Management
+            </label>
 
+            <div className="flex flex-wrap justify-around w-3/4 mt-4">
+                <Combobox
+                    as="div"
+                    value={
+                        supervisors
+                            .find((supervisor) => supervisor.id === incident.supervisor_id)
+                            ?.id.toString() ?? defaultText
+                    }
+                    onChange={(id) => {
+                        setQuery('');
+                        if (!id) return;
+                        const supervisor = supervisors.find(
+                            (supervisor) => supervisor.id === parseInt(id)
+                        );
+                        setModalProps((prev: ConfirmationModalProps) => ({
+                            ...prev,
+                            title: 'Assign Supervisor',
+                            text: `Are you sure you want to assign ${supervisor?.name} to this incident?\n${supervisor?.name} will be notified.`,
+                            action: () => assignSupervisor(parseInt(id), incident, setIsLoading),
+                            show: true,
+                        }));
+                    }}
+                >
+                    <div className="relative">
+                        <ComboboxInput
+                            className="block w-full rounded-md bg-white py-1.5 pl-3 pr-12 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                            onChange={(event) => setQuery(event.target.value)}
+                            onClick={() => setDefaultText('')}
+                            onBlur={() => {
+                                setDefaultText('Unassigned');
+                                setQuery('');
+                            }}
+                            displayValue={() => incident.supervisor?.name ?? defaultText}
+                        />
+                        <ComboboxButton className="absolute inset-y-0 right-0 flex items-center rounded-r-md px-2 focus:outline-none">
+                            <ChevronUpDownIcon
+                                className="size-5 text-gray-400"
+                                aria-hidden="true"
+                            />
+                        </ComboboxButton>
+
+                        {supervisors.length > 0 && (
+                            <ComboboxOptions className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
+                                {filteredSupervisors.map((supervisor) => (
+                                    <ComboboxOption
+                                        key={supervisor.id}
+                                        value={supervisor.id}
+                                        className="group relative cursor-default select-none py-2 pl-3 pr-9 text-gray-900 data-[focus]:bg-indigo-600 data-[focus]:text-white data-[focus]:outline-none"
+                                    >
+                                        <div className="">
+                                            <div className="group-data-[selected]:font-semibold">
+                                                {supervisor.name}
+                                            </div>
+                                            <div className="text-gray-500 group-data-[focus]:text-indigo-200">
+                                                {supervisor.email}
+                                            </div>
+                                        </div>
+
+                                        {supervisor.id === incident.supervisor_id && (
+                                            <span className="absolute flex inset-y-0 right-0 items-center pr-4 text-indigo-600 group-data-[focus]:text-white">
+                                                <CheckIcon className="size-5" aria-hidden="true" />
+                                            </span>
+                                        )}
+                                    </ComboboxOption>
+                                ))}
+                            </ComboboxOptions>
+                        )}
+                    </div>
+                </Combobox>
+                {isLoading ? (
+                    <LoadingIndicator className="h-6 w-6 text-gray-500" />
+                ) : incident.supervisor ? (
+                    <SecondaryButton
+                        onClick={() =>
                             setModalProps((prev: ConfirmationModalProps) => ({
                                 ...prev,
-                                title: 'Assign Supervisor',
-                                text: `Are you sure you want to assign ${supervisor?.name} to this incident?\n${supervisor?.name} will be notified.`,
-                                action: () =>
-                                    assignSupervisor(
-                                        parseInt(id),
-                                        incident,
-                                        setIsLoading
-                                    ),
+                                title: 'Unassign Supervisor',
+                                text: `Are you sure you want to unassign ${incident?.supervisor?.name} from this incident?`,
+                                action: () => unassignSupervisor(incident, setIsLoading),
                                 show: true,
-                            }));
-                        }}
-                        disabled={isLoading}
+                            }))
+                        }
                     >
-                        <div className="relative max-w-lg">
-                            <ListboxButton className="inline-flex items-center justify-between min-w-32 h-8 w-full cursor-default grid-cols-1 rounded-md bg-white pl-3 pr-2 text-left text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm">
-                                <span className="col-start-1 row-start-1 truncate pr-6">
-                                    {
-                                        supervisors.find(
-                                            (supervisor) =>
-                                                supervisor.id ===
-                                                incident.supervisor_id
-                                        )?.name
-                                    }
-                                </span>
-                                <ChevronUpDownIcon
-                                    aria-hidden="true"
-                                    className="col-start-1 row-start-1 size-5 self-center justify-self-end text-gray-500 sm:size-4"
-                                />
-                            </ListboxButton>
-                            <ListboxOptions
-                                transition
-                                className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none data-[closed]:data-[leave]:opacity-0 data-[leave]:transition data-[leave]:duration-100 data-[leave]:ease-in sm:text-sm"
-                            >
-                                {supervisors.map(
-                                    (supervisor) =>
-                                        supervisor.id !==
-                                            incident.supervisor_id && (
-                                            <ListboxOption
-                                                key={supervisor.id}
-                                                value={supervisor.id}
-                                                className="group relative cursor-default select-none py-2 pl-3 pr-9 text-gray-900 data-[focus]:bg-indigo-600 data-[focus]:text-white data-[focus]:outline-none"
-                                            >
-                                                {uppercaseWordFormat(
-                                                    supervisor.name
-                                                )}
-                                            </ListboxOption>
-                                        )
-                                )}
-                            </ListboxOptions>
-                        </div>
-                    </Listbox>
-                    {isLoading ? (
-                        <LoadingIndicator className="h-6 w-6 text-gray-500" />
-                    ) : incident.supervisor ? (
-                        <SecondaryButton
-                            onClick={() => {
-                                setModalProps(
-                                    (prev: ConfirmationModalProps) => ({
-                                        ...prev,
-                                        title: 'Unassign Supervisor',
-                                        text: `Are you sure you want to unassign ${incident?.supervisor?.name} from this incident?`,
-                                        action: () =>
-                                            unassignSupervisor(
-                                                incident,
-                                                setIsLoading
-                                            ),
-                                        show: true,
-                                    })
-                                );
-                            }}
-                        >
-                            Unassign
-                        </SecondaryButton>
-                    ) : (
-                        <></>
-                    )}
-                </div>
+                        Unassign
+                    </SecondaryButton>
+                ) : (
+                    <></>
+                )}
             </div>
             <ConfirmationModal
                 title={modalProps.title}
