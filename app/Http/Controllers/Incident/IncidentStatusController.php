93a69cffe7ccc5ea802dcca0234a849f00fba97e
Jonathon Meney <jonmeney@gmail.com>
2025-02-06 20:00:07 -0400

    removed supervisor assignment controller and moved into status controller

--- app/Http/Controllers/Incident/IncidentStatusController.php
+++ app/Http/Controllers/Incident/IncidentStatusController.php
@@ -9,6 +9,32 @@ use Illuminate\Http\Request;
 
 class IncidentStatusController extends Controller
 {
+    public function assignSupervisor(Request $request, Incident $incident)
+    {
+        $this->authorize('performAdminActions', Incident::class);
+
+        $form = $request->validate([
+            'supervisor_id' => 'required|exists:users,id',
+        ]);
+
+        IncidentAggregateRoot::retrieve($incident->id)
+            ->assignSupervisor($form['supervisor_id'])
+            ->persist();
+
+        return back();
+    }
+
+    public function unassignSupervisor(Request $request, Incident $incident)
+    {
+        $this->authorize('performAdminActions', Incident::class);
+
+        IncidentAggregateRoot::retrieve($incident->id)
+            ->unassignSupervisor()
+            ->persist();
+
+        return back();
+    }
+
     public function closeIncident(Request $request, Incident $incident)
     {
         $this->authorize('performAdminActions', Incident::class);
