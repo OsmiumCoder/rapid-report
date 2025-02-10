19ff84c1b2d5eca34707c867ff356f35010e1cc3
Jonathon Meney <jonmeney@gmail.com>
2025-02-09 20:39:42 -0400

    investigation aggregate root

--- app/Aggregates/InvestigationAggregateRoot.php
+++ app/Aggregates/InvestigationAggregateRoot.php
@@ -2,8 +2,26 @@
 
 namespace App\Aggregates;
 
+use App\Data\InvestigationData;
+use App\Models\Incident;
+use App\StorableEvents\Investigation\InvestigationCreated;
 use Spatie\EventSourcing\AggregateRoots\AggregateRoot;
 
 class InvestigationAggregateRoot extends AggregateRoot
 {
+    public function createInvestigation(InvestigationData $investigationData, Incident $incident)
+    {
+        $this->recordThat(new InvestigationCreated(
+            incident_id: $incident->id,
+            immediate_causes: $investigationData->immediate_causes,
+            basic_causes: $investigationData->basic_causes,
+            remedial_actions: $investigationData->remedial_actions,
+            prevention: $investigationData->prevention,
+            hazard_class: $investigationData->hazard_class,
+            risk_rank: $investigationData->risk_rank,
+            resulted_in: $investigationData->resulted_in,
+        ));
+
+        return $this;
+    }
 }
---
+++ tests/Unit/Aggregates/InvestigationAggregateRootTest.php
@@ -0,0 +1,204 @@
+<?php
+
+namespace Aggregates;
+
+use App\Aggregates\InvestigationAggregateRoot;
+use App\Data\InvestigationData;
+use App\Enum\CommentType;
+use App\Models\Incident;
+use App\Models\Investigation;
+use App\Models\User;
+use App\Notifications\Investigation\InvestigationSubmitted;
+use App\StorableEvents\Investigation\InvestigationCreated;
+use Illuminate\Support\Facades\Notification;
+use Illuminate\Support\Str;
+use Tests\TestCase;
+
+class InvestigationAggregateRootTest extends TestCase
+{
+    public function test_sends_received_notification_to_admin()
+    {
+        Notification::fake();
+
+        $supervisor = User::factory()->create()->assignRole('supervisor');
+        $this->actingAs($supervisor);
+
+        $admins = User::factory(3)->create()->each(function (User $user) {
+            $user->assignRole('admin');
+        });
+
+        $incident = Incident::factory()->create();
+
+        $investigationData = InvestigationData::from([
+            'immediate_causes' => "immediate causes",
+            'basic_causes' => 'basic causes',
+            'remedial_actions' => "remedial actions",
+            'prevention' => 'prevention',
+            'hazard_class' => 'hazard class',
+            'risk_rank' => 10,
+            'resulted_in' => ['injury', 'burn']
+        ]);
+
+        $uuid = Str::uuid()->toString();
+
+        Notification::assertNothingSent();
+
+        $aggregate = InvestigationAggregateRoot::retrieve($uuid)
+            ->createInvestigation($investigationData, $incident)
+            ->persist();
+
+        Notification::assertCount(3);
+
+        Notification::assertSentTo($admins, InvestigationSubmitted::class);
+
+        $investigation = Investigation::first();
+
+        Notification::assertSentTo(
+            $admins,
+            function (InvestigationSubmitted $notification, array $channels) use ($investigation, $supervisor) {
+                return $notification->investigationId === $investigation->id && $notification->supervisor->id === $supervisor->id;
+            }
+        );
+    }
+
+    public function test_adds_created_investigation_comment_on_incident()
+    {
+        $supervisor = User::factory()->create()->assignRole('supervisor');
+        $this->actingAs($supervisor);
+
+        $incident = Incident::factory()->create();
+
+        $investigationData = InvestigationData::from([
+            'immediate_causes' => "immediate causes",
+            'basic_causes' => 'basic causes',
+            'remedial_actions' => "remedial actions",
+            'prevention' => 'prevention',
+            'hazard_class' => 'hazard class',
+            'risk_rank' => 10,
+            'resulted_in' => ['injury', 'burn']
+        ]);
+
+        $uuid = Str::uuid()->toString();
+
+        $aggregate = InvestigationAggregateRoot::retrieve($uuid)
+            ->createInvestigation($investigationData, $incident)
+            ->persist();
+
+        $incident->refresh();
+
+        $this->assertCount(1, $incident->comments);
+
+        $comment = $incident->comments->first();
+
+        $this->assertEquals(CommentType::ACTION, $comment->type);
+        $this->assertStringContainsStringIgnoringCase('created', $comment->content);
+        $this->assertStringContainsStringIgnoringCase('investigation', $comment->content);
+
+    }
+
+    public function test_fires_investigation_created_event()
+    {
+        $supervisor = User::factory()->create()->assignRole('supervisor');
+        $this->actingAs($supervisor);
+
+        $incident = Incident::factory()->create();
+
+        $investigationData = InvestigationData::from([
+            'immediate_causes' => "immediate causes",
+            'basic_causes' => 'basic causes',
+            'remedial_actions' => "remedial actions",
+            'prevention' => 'prevention',
+            'hazard_class' => 'hazard class',
+            'risk_rank' => 10,
+            'resulted_in' => ['injury', 'burn']
+        ]);
+
+        InvestigationAggregateRoot::fake(Str::uuid()->toString())
+            ->when(function (InvestigationAggregateRoot $investigationAggregateRoot) use ($investigationData, $incident): void {
+                $investigationAggregateRoot->createInvestigation($investigationData, $incident);
+            })->assertRecorded([
+                new InvestigationCreated(
+                    incident_id: $incident->id,
+                    immediate_causes: "immediate causes",
+                    basic_causes: 'basic causes',
+                    remedial_actions: "remedial actions",
+                    prevention: "prevention",
+                    hazard_class: 'hazard class',
+                    risk_rank: 10,
+                    resulted_in: ['injury', 'burn'],
+                )
+            ]);
+    }
+
+    public function test_investigation_uuid_is_aggregate_uuid()
+    {
+        $supervisor = User::factory()->create()->assignRole('supervisor');
+        $this->actingAs($supervisor);
+
+        $incident = Incident::factory()->create();
+
+        $investigationData = InvestigationData::from([
+            'immediate_causes' => "immediate causes",
+            'basic_causes' => 'basic causes',
+            'remedial_actions' => "remedial actions",
+            'prevention' => 'prevention',
+            'hazard_class' => 'hazard class',
+            'risk_rank' => 10,
+            'resulted_in' => ['injury', 'burn']
+        ]);
+
+        $this->assertDatabaseCount('investigations', 0);
+
+        $uuid = Str::uuid()->toString();
+
+        $aggregate = InvestigationAggregateRoot::retrieve($uuid)
+            ->createInvestigation($investigationData, $incident)
+            ->persist();
+
+        $this->assertDatabaseCount('investigations', 1);
+
+        $investigation = Investigation::first();
+
+        $this->assertEquals($aggregate->uuid(), $investigation->id);
+    }
+
+    public function test_stores_investigation()
+    {
+        $supervisor = User::factory()->create()->assignRole('supervisor');
+        $this->actingAs($supervisor);
+
+        $incident = Incident::factory()->create();
+
+        $investigationData = InvestigationData::from([
+            'immediate_causes' => "immediate causes",
+            'basic_causes' => 'basic causes',
+            'remedial_actions' => "remedial actions",
+            'prevention' => 'prevention',
+            'hazard_class' => 'hazard class',
+            'risk_rank' => 10,
+            'resulted_in' => ['injury', 'burn']
+        ]);
+
+        $this->assertDatabaseCount('investigations', 0);
+
+        $uuid = Str::uuid()->toString();
+
+        $aggregate = InvestigationAggregateRoot::retrieve($uuid)
+            ->createInvestigation($investigationData, $incident)
+            ->persist();
+
+        $this->assertDatabaseCount('investigations', 1);
+
+        $investigation = Investigation::first();
+
+        $this->assertEquals($aggregate->uuid(), $investigation->id);
+        $this->assertEquals($incident->id, $investigation->incident_id);
+        $this->assertEquals($investigationData->immediate_causes, $investigation->immediate_causes);
+        $this->assertEquals($investigationData->basic_causes, $investigation->basic_causes);
+        $this->assertEquals($investigationData->remedial_actions, $investigation->remedial_actions);
+        $this->assertEquals($investigationData->prevention, $investigation->prevention);
+        $this->assertEquals($investigationData->hazard_class, $investigation->hazard_class);
+        $this->assertEquals($investigationData->risk_rank, $investigation->risk_rank);
+        $this->assertEquals($investigationData->resulted_in, $investigation->resulted_in);
+    }
+}
