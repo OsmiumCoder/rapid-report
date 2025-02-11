85d11299cc00df0141d720e1850e3ae00f0d358e
Jonathon Meney <jonmeney@gmail.com>
2025-02-10 21:40:32 -0400

    adds home button at end of create page to navigate to dashboard or login page

--- resources/js/Pages/Incident/Created.tsx
+++ resources/js/Pages/Incident/Created.tsx
@@ -33,6 +33,16 @@ export default function Created({
                         </Link>
                     </div>
                 )}
+
+                <div className="my-2">
+                    <Link
+                        href={route('login')}
+                        as="button"
+                        className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    >
+                        Home
+                    </Link>
+                </div>
             </div>
         </GuestLayout>
     );
