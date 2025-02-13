79f53f450d5d4301478f9de9b2b5eab420520eed
Jonathon Meney <jonmeney@gmail.com>
2025-02-13 13:58:01 -0400

    wip

--- resources/js/Pages/Incident/Index.tsx
+++ resources/js/Pages/Incident/Index.tsx
@@ -18,6 +18,7 @@ import { descriptors } from '@/Pages/Incident/Stages/IncidentDropDownValues';
 import { IncidentStatus } from '@/Enums/IncidentStatus';
 import Badge from '@/Components/Badge';
 import { incidentBadgeColor } from '@/Filters/incidentBadgeColor';
+import Pagination from "@/Components/Pagination";
 
 type IndexType = 'owned' | 'assigned' | 'all';
 
@@ -483,97 +484,8 @@ export default function Index({
                                         ))}
                                     </tbody>
                                 </table>
-                                <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 md:px-6">
-                                    <div className="flex flex-1 justify-between sm:hidden">
-                                        <Link
-                                            href={incidents.prev_page_url || '#'}
-                                            className={`relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50 ${
-                                                !incidents.prev_page_url
-                                                    ? 'cursor-not-allowed opacity-50'
-                                                    : ''
-                                            }`}
-                                        >
-                                            Previous
-                                        </Link>
-                                        <Link
-                                            href={incidents.next_page_url || '#'}
-                                            className={`relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50 ${
-                                                !incidents.next_page_url
-                                                    ? 'cursor-not-allowed opacity-50'
-                                                    : ''
-                                            }`}
-                                        >
-                                            Next
-                                        </Link>
-                                    </div>
-                                    <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
-                                        <div>
-                                            <p className="text-sm text-gray-700">
-                                                Showing{' '}
-                                                <span className="font-medium">
-                                                    {incidents.from}
-                                                </span>{' '}
-                                                to{' '}
-                                                <span className="font-medium">{incidents.to}</span>{' '}
-                                                of{' '}
-                                                <span className="font-medium">
-                                                    {incidents.total}
-                                                </span>{' '}
-                                                results
-                                            </p>
-                                        </div>
-                                        <div>
-                                            <nav
-                                                aria-label="Pagination"
-                                                className="isolate inline-flex -space-x-px rounded-md shadow-sm"
-                                            >
-                                                <Link
-                                                    href={incidents.prev_page_url || '#'}
-                                                    className={`relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 ${
-                                                        !incidents.prev_page_url
-                                                            ? 'cursor-not-allowed opacity-50'
-                                                            : ''
-                                                    }`}
-                                                >
-                                                    <ChevronLeftIcon
-                                                        className="h-5 w-5"
-                                                        aria-hidden="true"
-                                                    />
-                                                </Link>
 
-                                                {incidents.links.map((link, index) =>
-                                                    isNaN(Number(link.label)) ? null : (
-                                                        <Link
-                                                            key={index}
-                                                            href={link.url || '#'}
-                                                            className={`relative inline-flex items-center px-4 py-2 text-sm font-semibold ${
-                                                                link.active
-                                                                    ? 'z-10 bg-upei-green-500 text-white focus:z-20 focus:outline-offset-0'
-                                                                    : 'text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0'
-                                                            }`}
-                                                            preserveState={true}
-                                                        >
-                                                            {link.label}
-                                                        </Link>
-                                                    )
-                                                )}
-                                                <Link
-                                                    href={incidents.next_page_url || '#'}
-                                                    className={`relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 ${
-                                                        !incidents.next_page_url
-                                                            ? 'cursor-not-allowed opacity-50'
-                                                            : ''
-                                                    }`}
-                                                >
-                                                    <ChevronRightIcon
-                                                        className="h-5 w-5"
-                                                        aria-hidden="true"
-                                                    />
-                                                </Link>
-                                            </nav>
-                                        </div>
-                                    </div>
-                                </div>
+                                <Pagination pagination={incidents} />
                             </div>
                         </div>
                     </div>
