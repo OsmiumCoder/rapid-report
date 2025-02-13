f76d6db377607aca069411242d2795d84eaa1581
Jonathon Meney <jonmeney@gmail.com>
2025-02-13 16:22:36 -0400

    user factory assigns user role

---
+++ tests/Unit/Models/UserTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Models;
+
+use App\Models\User;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    public function test_creates_a_user_model_with_user_role()
+    {
+        $user = User::factory()->create();
+
+        $this->assertTrue($user->hasRole('user'));
+    }
+}
