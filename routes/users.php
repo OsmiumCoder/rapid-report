1f99cdf1784d6a9415520f9e9644e61c8b2a697f
Jonathon Meney <jonmeney@gmail.com>
2025-02-14 17:17:13 -0400

    user role update route and controller

--- routes/users.php
+++ routes/users.php
@@ -7,5 +7,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::resource('users', UserController::class)->only(['store', 'destroy']);
 
-    Route::patch('users/{user}/update-role', UserRoleController::class)->name('users.update-role');
+    Route::patch('users/{user}/update-role', [UserRoleController::class, 'update'])->name('users.update-role');
 });
