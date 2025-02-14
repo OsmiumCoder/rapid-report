1d017bf010d15418a1e2d23b39e25b3a1121ce1e
Jonathon Meney <jonmeney@gmail.com>
2025-02-14 17:15:59 -0400

    updated constructor of event for destroy

--- app/StorableEvents/User/UserDeleted.php
+++ app/StorableEvents/User/UserDeleted.php
@@ -8,13 +8,13 @@ use App\StorableEvents\StoredEvent;
 class UserDeleted extends StoredEvent
 {
     public function __construct(
-        public int $id,
+        public int $user_id,
     ) {
     }
 
     public function handle()
     {
-        $user = User::find($this->id);
+        $user = User::find($this->user_id);
 
         $user->delete();
     }
