7ea613b35d6f32541a821af84d05d55655ad402e
Jonathon Meney <jonmeney@gmail.com>
2025-02-13 21:37:44 -0400

    added routes

--- app/Http/Controllers/RootCauseAnalysis/RootCauseAnalysisController.php
+++ app/Http/Controllers/RootCauseAnalysis/RootCauseAnalysisController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\RootCauseAnalysis;
 
+use App\Http\Controllers\Controller;
 use App\Models\RootCauseAnalysis;
 use Illuminate\Http\Request;
 
