35bf44337edf435a1f54970799a9662cf00ec5a8
Jonathon Meney <jonmeney@gmail.com>
2025-02-17 22:12:18 -0400

    updates risk ranking modal to look correct

--- resources/js/Pages/Investigation/Partials/CreateComponents/RiskRankingModal.tsx
+++ resources/js/Pages/Investigation/Partials/CreateComponents/RiskRankingModal.tsx
@@ -1,5 +1,6 @@
 import Modal from '@/Components/Modal';
 import React, { Dispatch, SetStateAction } from 'react';
+import {XMarkIcon} from "@heroicons/react/24/outline";
 
 interface RiskRankingModalProps {
     isOpen: boolean;
@@ -7,76 +8,82 @@ interface RiskRankingModalProps {
 }
 export default function RiskRankingModal({ isOpen, setIsOpen }: RiskRankingModalProps) {
     return (
-        <Modal show={isOpen} onClose={() => setIsOpen(false)} maxWidth={'4xl'}>
-            <div className="p-4 flex flex-col justify-center text-center">
-                <h3 className="text-lg font-bold mb-2">
-                    Three Point Risk Ranking Scheme and Ranking Matrix
-                </h3>
+        <Modal show={isOpen} onClose={() => setIsOpen(false)} maxWidth={'5xl'}>
+            <div className="p-4 flex flex-col">
+                <div className="flex items-center justify-center text-lg font-bold mb-4">
+                    <div className="flex flex-1 justify-center">
+                        Three Point Risk Ranking Scheme and Ranking Matrix
+                    </div>
+                    <XMarkIcon onClick={() => setIsOpen(false)} className="flex justify-end size-6 cursor-pointer hover:text-gray-600"/>
+                </div>
 
-                <table className="border border-gray-300 text-lg">
-                    <thead>
-                        <tr>
-                            <th rowSpan={2} className="border border-gray-300 px-4 py-2">
-                                Frequency
-                            </th>
-                            <th colSpan={3} className="border border-gray-300 px-4 py-2">
-                                Hazard Severity
-                            </th>
-                        </tr>
-                        <tr>
-                            <th className="border border-gray-300 px-4 py-2">Minor</th>
-                            <th className="border border-gray-300 px-4 py-2">Serious</th>
-                            <th className="border border-gray-300 px-4 py-2">Major</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td className="border border-gray-300 px-4 py-2">Seldom</td>
-                            <td className="border-r border-b border-black px-4 py-2 bg-green-300">
-                                1
-                            </td>
-                            <td className="border-x border-b border-black px-4 py-2 bg-green-300">
-                                2
-                            </td>
-                            <td className="border-l border-b border-black px-4 py-2 bg-yellow-200">
-                                3
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className="border border-gray-300 px-4 py-2">Occasional</td>
-                            <td className="border-y border-r border-black px-4 py-2 bg-green-300">
-                                2
-                            </td>
-                            <td className="border border-black px-4 py-2 bg-yellow-200">4</td>
-                            <td className="border-y border-l border-black px-4 py-2 bg-red-300">
-                                6
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className="border border-gray-300 px-4 py-2">Frequent</td>
-                            <td className="border-t border-r border-black px-4 py-2 bg-yellow-200">
-                                3
-                            </td>
-                            <td className="border-t border-x border-black px-4 py-2 bg-red-300">
-                                6
-                            </td>
-                            <td className="border-t border-l border-black px-4 py-2 bg-red-300">
-                                9
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
+                <div className="grid gap-4 w-full text-center">
+                    <div className="w-full">
+                        <table className="w-full border border-gray-300 text-lg">
+                            <thead>
+                            <tr>
+                                <th rowSpan={2} className="border border-gray-300 px-4 py-2">
+                                    Frequency
+                                </th>
+                                <th colSpan={3} className="border border-gray-300 px-4 py-2">
+                                    Hazard Severity
+                                </th>
+                            </tr>
+                            <tr>
+                                <th className="border border-gray-300 px-4 py-2">Minor</th>
+                                <th className="border border-gray-300 px-4 py-2">Serious</th>
+                                <th className="border border-gray-300 px-4 py-2">Major</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td className="border border-gray-300 px-4 py-2">Seldom</td>
+                                <td className="border-r border-b border-black px-4 py-2 bg-green-300">
+                                    1
+                                </td>
+                                <td className="border-x border-b border-black px-4 py-2 bg-green-300">
+                                    2
+                                </td>
+                                <td className="border-l border-b border-black px-4 py-2 bg-yellow-200">
+                                    3
+                                </td>
+                            </tr>
+                            <tr>
+                                <td className="border border-gray-300 px-4 py-2">Occasional</td>
+                                <td className="border-y border-r border-black px-4 py-2 bg-green-300">
+                                    2
+                                </td>
+                                <td className="border border-black px-4 py-2 bg-yellow-200">4</td>
+                                <td className="border-y border-l border-black px-4 py-2 bg-red-300">
+                                    6
+                                </td>
+                            </tr>
+                            <tr>
+                                <td className="border border-gray-300 px-4 py-2">Frequent</td>
+                                <td className="border-t border-r border-black px-4 py-2 bg-yellow-200">
+                                    3
+                                </td>
+                                <td className="border-t border-x border-black px-4 py-2 bg-red-300">
+                                    6
+                                </td>
+                                <td className="border-t border-l border-black px-4 py-2 bg-red-300">
+                                    9
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
 
-                <div className="flex justify-between mt-6 gap-4">
-                    <table className="border border-gray-300 w-1/2">
-                        <thead>
+                    <div className="flex gap-4">
+                        <table className="border border-gray-300">
+                            <thead>
                             <tr>
                                 <th colSpan={2} className="border border-gray-300 px-4 py-2">
                                     Hazard Severity Potential
                                 </th>
                             </tr>
-                        </thead>
-                        <tbody>
+                            </thead>
+                            <tbody>
                             <tr>
                                 <td className="border border-gray-300 px-4 py-2">
                                     <strong>Minor</strong>
@@ -101,18 +108,17 @@ export default function RiskRankingModal({ isOpen, setIsOpen }: RiskRankingModal
                                     High risk to students/staff/visitors/contractors/UPEI
                                 </td>
                             </tr>
-                        </tbody>
-                    </table>
-
-                    <table className="border border-gray-300 w-1/2">
-                        <thead>
+                            </tbody>
+                        </table>
+                        <table className="border border-gray-300">
+                            <thead>
                             <tr>
                                 <th colSpan={2} className="border border-gray-300 px-4 py-2">
                                     Frequency / Probability of Occurrence
                                 </th>
                             </tr>
-                        </thead>
-                        <tbody>
+                            </thead>
+                            <tbody>
                             <tr>
                                 <td className="border border-gray-300 px-4 py-2 ">
                                     <strong>Seldom</strong>
@@ -139,8 +145,9 @@ export default function RiskRankingModal({ isOpen, setIsOpen }: RiskRankingModal
                                     the risk
                                 </td>
                             </tr>
-                        </tbody>
-                    </table>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </Modal>
