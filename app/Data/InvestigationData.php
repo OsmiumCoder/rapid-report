c6b76979a66379cb23b81348259f5cd8afb93937
Jonathon Meney <jonmeney@gmail.com>
2025-02-18 00:07:30 -0400

    made investigations array fields nullable

--- app/Data/InvestigationData.php
+++ app/Data/InvestigationData.php
@@ -13,11 +13,11 @@ class InvestigationData extends Data
         public string $prevention,
         public int $risk_rank,
         public array $resulted_in,
-        public array $substandard_acts,
-        public array $substandard_conditions,
-        public array $energy_transfer_causes,
-        public array $personal_factors,
-        public array $job_factors,
+        public ?array $substandard_acts,
+        public ?array $substandard_conditions,
+        public ?array $energy_transfer_causes,
+        public ?array $personal_factors,
+        public ?array $job_factors,
     ) {
     }
 }
