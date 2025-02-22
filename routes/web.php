e2e7e05a7a27186923974a67b0c5c7f4c434c991
Jonathon Meney <jonmeney@gmail.com>
2025-02-22 13:51:37 -0400

    lint

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,7 @@
 
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
 // TODO: Remove, used for demo purposes
 Route::get('/notification', function () {
