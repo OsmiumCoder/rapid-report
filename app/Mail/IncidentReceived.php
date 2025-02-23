853689c0103eb503155a8d4e803415c323cef5b4
tinfernn <tinfern2@proton.me>
2025-02-23 19:06:53 -0400

    Updated incident-received.blade.php to have the correct message.
    Updated IncidentCreated.php to receive an incident ID to link in the incident received email (if it should link to something different, then this can be changed later on)
    
    Updated IncidentReceived.php to pass in the incidentID. Again, if the email should link to something different, then this can be easily changed.

--- app/Mail/IncidentReceived.php
+++ app/Mail/IncidentReceived.php
@@ -13,6 +13,16 @@ class IncidentReceived extends Mailable
     use Queueable;
     use SerializesModels;
 
+    public string $incidentUrl;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct(public string $incidentUuid)
+    {
+        $this->incidentUrl = route('incidents.show', ['incident' => $this->incidentUuid]);
+    }
+
     /**
      * Get the message envelope.
      */
@@ -30,6 +40,9 @@ class IncidentReceived extends Mailable
     {
         return new Content(
             markdown: 'mail.incident-received',
+            with: [
+                'incidentUrl' => $this->incidentUrl,
+            ],
         );
     }
 
