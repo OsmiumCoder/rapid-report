7e3d94cc6603a2aa2aa70d6a538323ce4cf48f94
Liam Welsh <liamwelsh17@gmail.com>
2025-02-25 00:59:57 -0400

    Fixes confirmation modal z-index

--- resources/js/Components/ConfirmationModal/ConfirmationModal.tsx
+++ resources/js/Components/ConfirmationModal/ConfirmationModal.tsx
@@ -3,6 +3,7 @@ import PrimaryButton from '@/Components/PrimaryButton';
 import DangerButton from '@/Components/DangerButton';
 import { Dialog, DialogBackdrop, DialogPanel, DialogTitle } from '@headlessui/react';
 import { ExclamationTriangleIcon } from '@heroicons/react/16/solid';
+import ReactDOM from 'react-dom';
 
 export interface ConfirmationModalProps {
     title: string;
@@ -28,14 +29,14 @@ export default function ConfirmationModal({
 
     const handleNo = () => setShow(false);
 
-    return (
-        <Dialog open={show} onClose={() => {}} className="relative z-10">
+    return ReactDOM.createPortal(
+        <Dialog open={show} onClose={() => {}} className="relative z-50">
             <DialogBackdrop
                 transition
                 className="fixed inset-0 bg-gray-500/75 transition-opacity data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in"
             />
 
-            <div ref={modalRef} className="fixed inset-0 z-10 w-screen overflow-y-auto">
+            <div ref={modalRef} className="fixed inset-0 z-50 w-screen overflow-y-auto">
                 <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
                     <DialogPanel
                         transition
@@ -69,6 +70,7 @@ export default function ConfirmationModal({
                     </DialogPanel>
                 </div>
             </div>
-        </Dialog>
+        </Dialog>,
+        document.body
     );
 }
