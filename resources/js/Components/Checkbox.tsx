6559a59ceb8046f65125b615c0baf462dcfbc18b
Jonathon Meney <jonmeney@gmail.com>
2025-02-26 00:33:17 -0400

    removed todo

--- resources/js/Components/Checkbox.tsx
+++ resources/js/Components/Checkbox.tsx
@@ -1,12 +1,11 @@
 import { InputHTMLAttributes } from 'react';
 
-// TODO: Remove this component in favor of LabeledCheckbox
 export default function Checkbox({ className = '', ...props }: InputHTMLAttributes<HTMLInputElement>) {
     return (
         <input
             {...props}
             type="checkbox"
-            className={'text-upei-green-500 focus:ring-upei-green-600 rounded-sm border-gray-300 shadow-xs ' + className}
+            className={'rounded border-gray-300 text-upei-green-600 shadow-xs focus:ring-upei-green-500 ' + className}
         />
     );
 }
