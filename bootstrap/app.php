22a79dfae6401d81c52c4a6a1e1d0ed28fe95683
Jonathon Meney <jonmeney@gmail.com>
2025-02-27 14:05:11 -0400

    rate limit implemented

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,6 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
 
+        $middleware->throttleWithRedis();
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
