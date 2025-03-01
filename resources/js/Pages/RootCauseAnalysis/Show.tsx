442f7b41c20cd830678410f05ab213ca48355c98
Liam Welsh <liamwelsh17@gmail.com>
2025-03-01 15:18:33 -0400

    Fixes activity log taking up 2/3 of screen for supervisors

--- resources/js/Pages/RootCauseAnalysis/Show.tsx
+++ resources/js/Pages/RootCauseAnalysis/Show.tsx
@@ -1,11 +1,10 @@
-import classNames from '@/Filters/classNames';
 import Authenticated from '@/Layouts/AuthenticatedLayout';
+import ActivityLog from '@/Pages/Incident/Partials/ShowComponents/ActivityLog';
 import RootCauseAnalysisAdminActions from '@/Pages/RootCauseAnalysis/Partials/ShowComponents/RootCauseAnalysisAdminActions';
 import RootCauseAnalysisInformationPanel from '@/Pages/RootCauseAnalysis/Partials/ShowComponents/RootCauseAnalysisInformationPanel';
 import { Incident } from '@/types/incident/Incident';
 import { RootCauseAnalysis } from '@/types/rootCauseAnalysis/RootCauseAnalysis';
 import { Head, usePage } from '@inertiajs/react';
-import ActivityLog from "@/Pages/Incident/Partials/ShowComponents/ActivityLog";
 
 export default function Show({ rca }: { rca: RootCauseAnalysis; incident: Incident }) {
     const { user } = usePage().props.auth;
@@ -15,19 +14,10 @@ export default function Show({ rca }: { rca: RootCauseAnalysis; incident: Incide
             <Head title="Root Cause Analysis" />
             <main>
                 <div className="mx-auto px-4 py-10 sm:px-6 lg:px-8">
-                    <div
-                        className={classNames(
-                            'mx-auto grid max-w-2xl grid-cols-1 grid-rows-1 items-start gap-x-8 gap-y-8 lg:mx-0 lg:max-w-none',
-                            user.roles.some(({ name }) => name === 'supervisor') ? 'lg:grid-cols-1' : 'lg:grid-cols-3',
-                        )}
-                    >
+                    <div className="mx-auto grid max-w-2xl grid-cols-1 grid-rows-1 items-start gap-x-8 gap-y-8 lg:mx-0 lg:max-w-none lg:grid-cols-3">
                         {user.roles.some((role) => role.name === 'admin') && <RootCauseAnalysisAdminActions rca={rca} />}
                         <RootCauseAnalysisInformationPanel rca={rca} />
-                        {user.roles.some((role) => role.name === 'admin' || role.name === 'supervisor') && (
-                            <ActivityLog
-                                incident={rca.incident}
-                            />
-                        )}
+                        {user.roles.some((role) => role.name === 'admin' || role.name === 'supervisor') && <ActivityLog incident={rca.incident} />}
                     </div>
                 </div>
             </main>
