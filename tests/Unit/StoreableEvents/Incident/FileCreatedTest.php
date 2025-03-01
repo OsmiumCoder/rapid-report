3571635509927f38c8369d4cb0f3a7fed16b377d
Jonathon Meney <jonmeney@gmail.com>
2025-03-01 13:25:19 -0400

    file created event

---
+++ tests/Unit/StoreableEvents/Incident/FileCreatedTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace StoreableEvents\Incident;
+
+use App\Models\File;
+use App\Models\Incident;
+use App\StorableEvents\Incident\FileCreated;
+use Tests\TestCase;
+
+class FileCreatedTest extends TestCase
+{
+    public function test_stored_file_attached_to_incident()
+    {
+        $incident = Incident::factory()->create();
+
+        $event = new FileCreated(
+            name: 'stored-path-name',
+            original_name: 'file.pdf',
+            path: $incident->id,
+            size: '100',
+            mime_type: 'application/pdf',
+            extension: 'pdf',
+            fileable_id: $incident->id,
+            fileable_type: Incident::class
+        );
+
+        $this->assertDatabaseCount('files', 0);
+
+        $event->handle();
+
+        $this->assertDatabaseCount('files', 0);
+
+        $file = File::first();
+
+        $this->assertEquals($incident->id, $file->fileable->id);
+    }
+
+    public function test_file_model_stored_to_database()
+    {
+        $incident = Incident::factory()->create();
+        $event = new FileCreated(
+            name: 'stored-path-name',
+            original_name: 'file.pdf',
+            path: $incident->id,
+            size: '100',
+            mime_type: 'application/pdf',
+            extension: 'pdf',
+            fileable_id: $incident->id,
+            fileable_type: Incident::class
+        );
+
+        $this->assertDatabaseCount('files', 0);
+
+        $event->handle();
+
+        $this->assertDatabaseCount('files', 0);
+
+        $file = File::first();
+
+        $this->assertEquals('stored-path-name', $file->name);
+        $this->assertEquals('file.pdf', $file->original_name);
+        $this->assertEquals($incident->id, $file->path);
+        $this->assertEquals('100', $file->size);
+        $this->assertEquals('application/pdf', $file->mime_type);
+        $this->assertEquals('pdf', $file->extension);
+    }
+}
