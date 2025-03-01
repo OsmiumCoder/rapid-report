7091c64ea7ccd73281c2862c5fc414cec9cdd68b
Liam Welsh <liamwelsh17@gmail.com>
2025-03-01 15:39:02 -0400

    Updates File interface

--- resources/js/types/file/File.ts
+++ resources/js/types/file/File.ts
@@ -1,8 +1,9 @@
 import { FileExtension } from '@/types/file/FileExtension';
-
 export interface File {
+    name: string;
     original_name: string;
+    path: string;
+    size: number;
+    mime_type: string;
     extension: FileExtension;
-    url: string;
-    created_at: string;
 }
