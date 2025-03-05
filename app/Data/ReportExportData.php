98a80b5556e6f6ca1a884e6a2b9fea6b4728bdd3
Jonathon Meney <jonmeney@gmail.com>
2025-03-05 17:33:28 -0400

    removed booleans on data

--- app/Data/ReportExportData.php
+++ app/Data/ReportExportData.php
@@ -2,27 +2,74 @@
 
 namespace App\Data;
 
+use Carbon\CarbonImmutable;
+use Illuminate\Validation\Rule;
+use Spatie\LaravelData\Attributes\MergeValidationRules;
+use Spatie\LaravelData\Attributes\WithCast;
+use Spatie\LaravelData\Casts\DateTimeInterfaceCast;
 use Spatie\LaravelData\Data;
+use Spatie\LaravelData\Support\Validation\ValidationContext;
 
+#[MergeValidationRules]
 class ReportExportData extends Data
 {
     public function __construct(
-        public string $start,
-        public string $end,
-        public bool   $happened_at = false,
-        public bool   $work_related = false,
-        public bool   $personal_individual_information = false,
-        public bool   $workers_comp_submitted = false,
-        public bool   $location = false,
-        public bool   $room_number = false,
-        public bool   $incident_type = false,
-        public bool   $descriptor = false,
-        public bool   $description = false,
-        public bool   $injury_description = false,
-        public bool    $first_aid_description = false,
-        public bool    $closed_at = false,
-        public bool $created_at = false,
-        public bool $updated_at = false,
+        #[WithCast(DateTimeInterfaceCast::class)]
+        public CarbonImmutable $start,
+        #[WithCast(DateTimeInterfaceCast::class)]
+        public CarbonImmutable $end,
+        public array $fields
     ) {
+        //        slug
+        //        happened_at
+        //        location
+        //        room_number
+        //        incident_type
+        //        descriptor
+        //        description
+        //        injury_description
+        //        first_aid_description
+        //        created_at
+        //        status
+        //        closed_at
+
+        //        anonymous
+        //        on_behalf
+        //        on_behalf_anonymous
+        //        role
+        //        work_related
+        //        workers_comp_submitted
+
+        //        last_name
+        //        first_name
+        //        upei_id
+        //        email
+        //        phone
+        //        witnesses
+        //        reporters_email
+        //        supervisor_name
+
+        //        additional_information
+    }
+
+    public static function rules(ValidationContext $context): array
+    {
+        return [
+            'fields' => ['min:1', 'distinct'],
+            'fields.*' => [Rule::in([
+                'slug',
+                'happened_at',
+                'location',
+                'room_number',
+                'incident_type',
+                'descriptor',
+                'description',
+                'injury_description',
+                'first_aid_description',
+                'created_at',
+                'status',
+                'closed_at',
+            ])],
+        ];
     }
 }
