d49e9516066bba33b9ceb8cf5356b19780769b55
Sharif <sabdalla11124@upei.ca>
2025-03-05 13:49:24 -0400

    Added file uploader name to incident files Made UPEI Icon route to dashboard Removed victim information section, put divs in general description Fixed incident information twice to incident information continued

--- resources/js/Layouts/Partials/DesktopSidebar.tsx
+++ resources/js/Layouts/Partials/DesktopSidebar.tsx
@@ -1,6 +1,6 @@
 import ApplicationLogo from '@/Components/ApplicationLogo';
 import NavigationItems from '@/Layouts/Partials/NavigationItems';
-import { Link } from '@inertiajs/react';
+import {Link} from "@inertiajs/react";
 
 export default function DesktopSidebar() {
     return (
@@ -8,9 +8,7 @@ export default function DesktopSidebar() {
             <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
                 <div className="bg-upei-red-500 flex grow flex-col gap-y-5 overflow-y-auto px-6 pb-4">
                     <div className="flex items-center justify-center">
-                        <Link as="div" className="w-[50%]" href={route('dashboard')}>
-                            {' '}
-                            <ApplicationLogo />{' '}
+                        <Link as='div' className="w-[50%]" href={route('dashboard')}> <ApplicationLogo/>
                         </Link>
                     </div>
 
