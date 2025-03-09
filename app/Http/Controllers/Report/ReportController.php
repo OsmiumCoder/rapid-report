dd2f05c16b00489f861747bbc8221c399a9dab48
Jonathon Meney <jonmeney@gmail.com>
2025-03-09 14:20:51 -0300

    small changes

--- app/Http/Controllers/Report/ReportController.php
+++ app/Http/Controllers/Report/ReportController.php
@@ -4,47 +4,57 @@ namespace App\Http\Controllers\Report;
 
 use App\Http\Controllers\Controller;
 use App\Models\Incident;
+use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Support\Facades\Gate;
 use Inertia\Inertia;
+use Inertia\Response;
 
 class ReportController extends Controller
 {
-    public function index()
+    /**
+     * @throws AuthorizationException
+     */
+    public function index(): Response
     {
         Gate::authorize('view-report-page');
 
         return Inertia::render('Report/Index');
     }
 
-    public function stats()
+    /**
+     * @throws AuthorizationException
+     */
+    public function stats(): Response
     {
         Gate::authorize('view-report-page');
-        $pairs = Incident::selectRaw('created_at')
-            ->addSelect('closed_at')
+
+        $pairs = Incident::select(['created_at', 'closed_at'])
             ->whereNotNull('closed_at')
             ->get(['created_at', 'closed_at'])
             ->toArray();
-        $diffs = array_map(fn($item):false|int =>
-            date_diff(date_create($item['created_at']),date_create($item['closed_at']))->days, $pairs);
-        $count = array_count_values(array_reduce($diffs, function($carry, $value) {
+
+        $diffs = array_map(fn ($item): false|int =>
+            date_diff(date_create($item['created_at']), date_create($item['closed_at']))->days, $pairs);
+
+        $count = array_count_values(array_reduce($diffs, function ($carry, $value) {
             if ($value < 1) {
                 $carry[] = 'Less then 1 day ';
-            } elseif ($value >= 1 && $value < 7) {
+            } elseif ($value < 7) {
                 $carry[] = 'Between 1 day and a Week';
-            } elseif ($value >= 7 && $value < 31) {
+            } elseif ($value < 31) {
                 $carry[] = 'Between 1 week and a Month';
-            } elseif ($value >= 31 ) {
+            } else {
                 $carry[] = 'Over a month';
             }
             return $carry;
         }, []));
 
         return Inertia::render('Report/Stats', [
-            'location' => Incident::selectRaw('location ,count(location) as total')
+            'location' => Incident::selectRaw('location, count(location) as total')
                 ->groupBy('location')
                 ->pluck('total', 'location'),
             'incident_live_time_dist' => $count,
-            'type_dist' => Incident::selectRaw('incident_type ,count(incident_type) as total')
+            'type_dist' => Incident::selectRaw('incident_type, count(incident_type) as total')
                 ->groupBy('incident_type')
                 ->pluck('total', 'incident_type'),
             'role_dist' => Incident::selectRaw('role, count(role) as total')
@@ -59,15 +69,15 @@ class ReportController extends Controller
             'descriptor_dist' => Incident::selectRaw('descriptor, count(descriptor) as total')
                 ->groupBy('descriptor')
                 ->pluck('total', 'descriptor'),
-            'safety_dist' => Incident::selectRaw('descriptor,count(descriptor) as total')
+            'safety_dist' => Incident::selectRaw('descriptor, count(descriptor) as total')
                 ->where('incident_type', 1)
                 ->groupBy('descriptor')
                 ->pluck('total', 'descriptor'),
-            'environmental_dist' => Incident::selectRaw('descriptor,count(descriptor) as total')
+            'environmental_dist' => Incident::selectRaw('descriptor, count(descriptor) as total')
                 ->where('incident_type', 2)
                 ->groupBy('descriptor')
                 ->pluck('total', 'descriptor'),
-            'security_dist' => Incident::selectRaw('descriptor,count(descriptor) as total')
+            'security_dist' => Incident::selectRaw('descriptor, count(descriptor) as total')
                 ->where('incident_type', 3)
                 ->groupBy('descriptor')
                 ->pluck('total', 'descriptor'),
