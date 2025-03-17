781648064ad940e1c3a5f917bdaaf58496d98d9f
Jonathon Meney <jonmeney@gmail.com>
2025-03-17 18:39:58 -0300

    tenant orginization

--- config/services.php
+++ config/services.php
@@ -39,6 +39,8 @@ return [
         'client_id' => env('MICROSOFT_CLIENT_ID'),
         'client_secret' => env('MICROSOFT_CLIENT_SECRET'),
         'redirect' => '/auth/callback',
+        'tenant' => 'organizations',
+        'include_tenant_info' => true,
     ],
 
 ];
